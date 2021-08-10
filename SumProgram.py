242bcc2e1eb2de2ce7daa0dae818587cad4f03a5
Thrihesh Gandi <gandhi.thrihesh@gmail.com>
2021-08-10 00:09:21 -0700

    updated

--- SumProgram.py
+++ SumProgram.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 import os
 import string
@@ -10,25 +11,25 @@ def main():
     print("Argument List:", str(sys.argv))
  
     if len(sys.argv) <= 1:
-        print("Arguments are missing")
+        sys.stderr.write("Arguments are missing")
     elif str (sys.argv[1]) == '-f' or str (sys.argv[1]) == '-F':
         print("Input is a File")
         if len(sys.argv) <= 2:
-            print("File Name does not present")
+            sys.stderr.write("File Name does not present")
         else:
             print("file string exists and verifying the path")
             if str(path.exists(str (sys.argv[2]))) == "True":
-                print("File is present in the path")
+                #print("File is present in the path")
                 fileContent = open(str (sys.argv[2]), "r")
                 sum = 0
                 for line in fileContent:
                     for x in line:
                         if x.isdigit():
                             sum = sum + int(x)
-                print("sum of all Hexa deciamals in Decimals is: ", sum)
+                print("sum of all digits is: ", sum)
 
             else:
-                print("File does not exist")
+                sys.stderr.write("File does not exist")
     elif str (sys.argv[1]) == '-x' or str (sys.argv[1]) == '-X':
         print("input is hexa decimal")
         if len(sys.argv) <= 2:
@@ -41,7 +42,7 @@ def main():
                 found = c in string.hexdigits
                 if found == True:
                     sum = sum + int(c,16)
-            print("sum is:", sum)  
+            print("sum of all hexa decimals is:", sum)  
 
 
     else:
@@ -51,11 +52,9 @@ def main():
         sum = 0
         for char in input_string:
             if char.isdigit():
-                print("yes this is int")
+                #print("yes this is int")
                 sum = sum + int(char)
-            else:
-                print("not an int")
-        print("sum is:", sum)
+        print("sum of all digits in a string:", sum)
 
 
 if __name__ == "__main__":
